015020174bd22bd9352ffe8207b9c1f2e51c993e
Fernando <fperez.personal@gmail.com>
2020-09-24 00:33:47 -0300

    update

--- lightdm-screenshot.sh
+++ lightdm-screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 10s; DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/$DISPLAY xwd -root > /tmp/greeter.xwd
-convert /tmp/greeter.xwd /home/fernando/greeter.png
+convert /tmp/greeter.xwd /home/nando/greeter.png
